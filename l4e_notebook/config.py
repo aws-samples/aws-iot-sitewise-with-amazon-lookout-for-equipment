f1f159affcc2fc91e1e6814e8668af20c8f75caa
JuliaHu <70712578+Julia-Bobo-Hu@users.noreply.github.com>
2021-08-16 00:24:51 -0500

    initial commit

---
+++ l4e_notebook/config.py
@@ -0,0 +1,15 @@
+# Update the name of the bucket you want to use
+# to store the intermediate results of this getting
+# started:
+
+BUCKET                   = 'l4e-sitewise-d7bf5fa0'
+
+# You can leave these other parameters to these
+# default values:
+
+PREFIX_TRAINING          = '97978334-97be-4e45-bf5d-84d9e535a601/training-data/'
+PREFIX_LABEL             = '97978334-97be-4e45-bf5d-84d9e535a601/label-data/'
+PREFIX_INFERENCE         = '97978334-97be-4e45-bf5d-84d9e535a601/inference-data'
+DATASET_NAME             = '97978334-97be-4e45-bf5d-84d9e535a601'
+MODEL_NAME               = f'{DATASET_NAME}-model'
+INFERENCE_SCHEDULER_NAME = f'{DATASET_NAME}-scheduler'
